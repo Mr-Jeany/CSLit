14358e2d64fce48f96d28ab86cb50a183ebf54cc
Jeany <72220793+Mr-Jeany@users.noreply.github.com>
2023-10-06 10:35:15 +0300

    Update 06-10.cs
    Task 3, started task 4

--- 06-10.cs
+++ 06-10.cs
@@ -9,7 +9,7 @@ Write your code in this editor and press "Run" button to execute it.
 using System;
 class HelloWorld {
   static void Main() {
-      Task2();
+      Task4();
   }
   
   static void Task2() {
@@ -48,9 +48,53 @@ class HelloWorld {
                               return;
                           }
                       }
-                  }
+                  } else { Console.WriteLine("NOT FOUND!"); return; }
               }
           }
       }
+  }
+  
+  static void Task3() {
+      string text = Console.ReadLine();
+      string[] words = text.Split();
+      Console.WriteLine("The last word is \"" + words[words.Length-1] + "\". It's length is " + words[words.Length-1].Length);
+  }
+  
+  static void Task4() {
+      string text = Console.ReadLine();
+      int number = int.Parse(text);
+      
+      int[] numbers = new int[text.Length];
+      
+      for (int i = 0; i < text.Length; i++) {
+          numbers[i] = number % 10;
+          number = number / 10;
+      }
+      
+      string[] strNum = new string[numbers.Length];
+      
+      for (int i = 0; i < numbers.Length; i++) {
+          strNum[i] = numbers[i].ToString();
+      }
+      
+      for (int i = 0; i < numbers.Length; i++) {
+          if (numbers[i] < 10) {
+              if (numbers[i] <= 3 && numbers[i] >= 1) {
+                  strNum[Array.IndexOf(strNum, numbers[i])] = MultStr("I", numbers[i]);
+              } else if (4  <= numbers[i] && number <= 8) {
+                  strNum[Array.IndexOf(strNum, numbers[i])] = "V" + MultStr("I", numbers[i]-5);
+              } else {
+                  
+              }
+          }
+      }
+  }
+  
+  static string MultStr(string str, int mult) {
+      string tempStr = "";
+      for (int n = 0; n < mult; n++) {
+          tempStr = tempStr + str;
+      }
+      return tempStr;
   }
 }
